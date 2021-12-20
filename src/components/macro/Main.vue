c3170adb9e6dc7cad6363345adef0a990a7864b6
Alessandro-Rega <regaale47@gmail.com>
2021-12-20 22:32:02 +0100

    insert script flags and vote

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -29,17 +29,26 @@ export default {
                 params: {
                 api_key: "aa336647215e508c9724202ef49eaffc",
                 query: cerca,
-                language: "it-IT",
                 },
             })
             .then((response) => {
                 this.arrayFilm = response.data.results;
+                this.stelline();
                 console.log(response.data.results);
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             });
+        },
+        stelline(){
+            for(let i = 0; i < this.arrayFilm.length; i++){
+                this.arrayFilm[i].vote_average /= 2;
+                const int_part = Math.trunc(this.arrayFilm[i].vote_average);
+                const float_part = Number((this.arrayFilm[i].vote_average-int_part).toFixed(2));
+                if(float_part >= 0.5)this.arrayFilm[i].vote_average = parseInt(this.arrayFilm[i].vote_average) + 1;
+                else this.arrayFilm[i].vote_average = parseInt(this.arrayFilm[i].vote_average);
+            }
         }
     }
 }
--- src/components/sections/Film.vue
+++ src/components/sections/Film.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="film_card">
-      <img :src="'https://image.tmdb.org/t/p/w342' + film.poster_path" alt="">
+      <img v-if="film.poster_path != null" :src="'https://image.tmdb.org/t/p/w342' + film.poster_path" alt="poster">
       <span>{{film.title}}</span>
       <span>{{film.original_title}}</span>
-      <span>{{film.original_language}}</span>
+      <img v-if="film.original_language == 'it'" src="../../assets/flags/italy.png" alt="lingua">
+      <img v-if="film.original_language == 'en'" src="../../assets/flags/uk.png" alt="lingua">
+      <img v-if="film.original_language == 'es'" src="../../assets/flags/spain.png" alt="lingua">
+      <img v-if="film.original_language == 'fr'" src="../../assets/flags/france.png" alt="lingua">
+      <img v-if="film.original_language == 'gr'" src="../../assets/flags/germany.png" alt="lingua">
+      <img v-if="film.original_language == 'zh'" src="../../assets/flags/japan.png" alt="lingua">
       <span>{{film.vote_average}}</span>
   </div>
 </template>
@@ -13,7 +18,7 @@ export default {
     name: 'Film',
     props:{
         film: Object,
-    }
+    },
 }
 </script>
 
